e89ab6d576f05b91900b1f84ebbd67bc1ac9970c
Stephanie Sadowski <stephaniesadowski@Stephanies-Air.home>
2022-07-25 22:23:00 -0400

    updated calculator-design

--- calculator/calc2.js
+++ calculator/calc2.js
@@ -1,18 +1,14 @@
 const outputScreen = document.querySelector('.output')
 const firstNumberEntered = document.querySelector('.first-num')
 const secondNumberEntered = document.querySelector('.second-num')
-const buttonAC = document.querySelector('.btn-ac')
-const buttonDelete = document.querySelector('.btn-del')
-const buttonEqual = document.querySelector('#btn-equal')
-const buttonPercent = document.querySelector('.btn-percent') 
-const buttonNumberKey = document.querySelector('.number-key')
 const operation = document.querySelectorAll('.btn-operation')
 
-class Calculator {
-   constructor(firstNumberEntered, secondNumberEntered) {
-     this.firstNumberEntered = firstNumberEntered
-     this.secondNumberEntered = secondNumberEntered
-   }
+function buttonAction() {
+   document.querySelector('btn-action')
+}
+
+function buttonNumberKey() {
+   document.querySelector('number-key')
 }
 
 function addOperation(x, y)      {return x + y}
@@ -25,14 +21,11 @@ if (operation === '-') {console.log(subtractOperation(Number(firstNumberEntered)
 if (operation === '*') {console.log(multiplyOperation(Number(firstNumberEntered)), (Number(secondNumberEntered)))}
 if (operation === '/') {console.log(divideOperation(Number(firstNumberEntered)), (Number(secondNumberEntered)))}
 
-buttonEqual.addEventListener('click', (button) => {
-   Calculator.?
-})
+
 
 /* Things to figure out:
    Getting the output to display in display area
-   Use class contructor? Don't fully understand why. If it's initiating an object what object am I trying to initiate?
-   Is it so I can use the addEventListener method for my onlicks?
-
+   Get buttons working
+   How to get it so decimal can only be clicked once in a number
 
 */
